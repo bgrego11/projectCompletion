eef9911fb30223f47356eb189e394c9ee5daa609
StevenS125 <sts12589@gmail.com>
2019-08-12 14:42:28 -0400

    added express server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.use(express.static('css'))
+
+app.use('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.listen(8080);
