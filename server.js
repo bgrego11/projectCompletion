95d1d4b0b9f4af7f5a298d8ddf7ea9e0b1739759
StevenS125 <sts12589@gmail.com>
2019-08-22 14:25:15 -0400

    added services and contact routes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var app = express()
-var Router = express.Router();
 var port = process.env.PORT || 8080;
 
 // viewed at http://localhost:8080
